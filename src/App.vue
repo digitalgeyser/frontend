a1c763ba274b7b4cfddb13bad8563e233e53ff30
Timotej Ecimovic <tecimovic@gmail.com>
2026-06-13 23:41:19 -0400

    First pass. (#2)
    * Work in progress html.
    
    * Get the first version.

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <main>
+      <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+      <ResearchSection />
+      <ProcessSection />
+      <ContactSection />
+    </main>
+    <FooterSection />
+  </div>
+</template>
+
+<script setup>
+import NavBar from './components/NavBar.vue'
+import HeroSection from './components/HeroSection.vue'
+import AboutSection from './components/AboutSection.vue'
+import ServicesSection from './components/ServicesSection.vue'
+import ResearchSection from './components/ResearchSection.vue'
+import ProcessSection from './components/ProcessSection.vue'
+import ContactSection from './components/ContactSection.vue'
+import FooterSection from './components/FooterSection.vue'
+</script>
